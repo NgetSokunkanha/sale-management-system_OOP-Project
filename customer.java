c7e8d2ce3e7c27611fe0a7da788bbcd081b2e1e5
Nuttsotea Tireachh <85043671+NUTTSOTEATIREACHH@users.noreply.github.com>
2026-02-02 14:56:04 +0700

    Added new functions

--- customer.java
+++ customer.java
@@ -1,20 +1,37 @@
+import java.util.Random; // To generate order ID
+
 public class customer {
     String customername;
-    String name;
+    String email;
+    String phone;
+    String address;
 
-    // Constructor
-    public customer(String customername, String name) {
+    public customer(String customername, String phone, String email, String address) {
         this.customername = customername;
-        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
     }
-
-    // Some Functions that can be used
+    
     public String getCustomerUsername() {
         return customername;
     }
 
-    public String getRealName() {
-        return name;
+    public String getPhone() {
+        return phone;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int generateOrderID() {
+        Random rand = new Random();
+        int orderID = rand.nextInt(1000000); // From 0 to 999999
+        return orderID;
+    }
 }
